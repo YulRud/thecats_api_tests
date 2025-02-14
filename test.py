aceea26ee22986fe04ce02188579b03a57be613c
YulRud <yuliia.rudiak@gmail.com>
2025-02-14 15:42:35 +0200

    refactoring

--- test.py
+++
@@ -1,7 +0,0 @@
-from client.cat_favorites_client import CatFavoritesClient
-
-client = CatFavoritesClient()
-
-response = client.get_favorites('live_Qc2yvxGD20H7cPuXf2TMWoxZmGT25UVZyPKHwR5vIlxLvmjLvx01PwXo8rXYu78O')
-print(response.status_code)
-print(response.json())
